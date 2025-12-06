d20deda2c661801041838a1d1eaac0a0186aeb5a
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-06 16:21:21 +0530

    Add files via upload

---
+++ ac1.c
@@ -0,0 +1,13 @@
+//1.Adding two numbers.
+
+#include <stdio.h>
+int main(){
+    int x,y;
+    printf("Enter Number 1:");
+    scanf("%d",&x);
+    printf("Enter Number 2:");
+    scanf("%d",&y);
+    printf("%d",x+y);
+    return 0;
+}
+
