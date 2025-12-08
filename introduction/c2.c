32004cbf67a853c7d54d1938b655cd6dbbd0dd99
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-08 09:39:01 +0530

    Add files via upload

---
+++ introduction/c2.c
@@ -0,0 +1,16 @@
+// C Level 2 – Problem 1
+// Check if the number equals 50
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+
+    if (num == 50)
+        printf("1");
+    else
+        printf("0");
+
+    return 0;
+}
