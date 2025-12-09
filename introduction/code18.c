7c71ac7a6ee2c535d597804fe8f9ea4e97a436c5
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-09 15:00:12 +0530

    Add files via upload

---
+++ introduction/code18.c
@@ -0,0 +1,11 @@
+// Program: Get a three-digit number and make the one's digit 2
+
+#include <stdio.h>
+int main() {
+    int num, result;
+    printf("Enter a three-digit number: ");
+    scanf("%d", &num);
+    result = (num / 10) * 10 + 2;   // remove last digit and add 2
+    printf("Result: %d\n", result);
+    return 0;
+}
