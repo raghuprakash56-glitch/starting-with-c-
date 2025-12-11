df208433be1af44a8b8019beb8be7b2fddb40525
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-11 09:14:43 +0530

    Add files via upload

---
+++ relational check program/code11.c
@@ -0,0 +1,16 @@
+ //Check if digit 1 is less than or equal to digit 0
+
+#include <stdio.h>
+int main() {
+    int num, d0, d1;
+    printf("Enter a two-digit number: ");
+    scanf("%d", &num);
+    d0 = num / 10;   // Tens digit
+    d1 = num % 10;   // Ones digit
+    if(d1 <= d0) {
+        printf("1");
+    } else {
+        printf("0");
+    }
+    return 0;
+}
