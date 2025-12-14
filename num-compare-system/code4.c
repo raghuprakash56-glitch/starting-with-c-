20ddc8a4bc84bd94f3c131b3b212baabc7edcfe2
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-15 00:17:45 +0530

    Add files via upload

---
+++ num-compare-system/code4.c
@@ -0,0 +1,14 @@
+// Check if the number is greater than 50
+
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num > 50) {
+        printf("1");
+    } else {
+        printf("0");
+    }
+    return 0;
+}
