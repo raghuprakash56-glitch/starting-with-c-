3a611a43daf35e6877cffb1ddd10a7c2a14806c3
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-15 11:25:49 +0530

    Add files via upload

---
+++ relation condition program/code01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Program to Check if the Number Equals 50
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num == 50) {
+        printf("Success\n");
+    } else {
+        printf("Failure\n");
+    }
+
+    return 0;
+}
