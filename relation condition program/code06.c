369193866b6fa573ec857b3a4269de0009ea996c
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-15 11:27:39 +0530

    Add files via upload

---
+++ relation condition program/code06.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Program to Check if the Number is Greater Than or Equal to 50
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num >= 50) {
+        printf("Success\n");
+    } else {
+        printf("Failure\n");
+    }
+
+    return 0;
+}
