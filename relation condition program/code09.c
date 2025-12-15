b8eb144a9a977e4aa6e74b0e9e06e989018fa105
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-15 11:28:58 +0530

    Add files via upload

---
+++ relation condition program/code09.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Program to Check if Digit 0 Is Less Than Digit 1
+    int num;
+    int d1, d0;
+
+    printf("Enter a two-digit number: ");
+    scanf("%d", &num);
+
+    d0 = num % 10;      // digit 0 (ones place)
+    d1 = num / 10;      // digit 1 (tens place)
+
+    if (d0 < d1) {
+        printf("Success\n");
+    } else {
+        printf("Failure\n");
+    }
+
+    return 0;
+}
