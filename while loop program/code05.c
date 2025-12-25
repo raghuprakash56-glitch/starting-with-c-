10d3e6f8d5910f025edb2c9ec98671fb3482da01
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-25 23:07:37 +0530

    Add files via upload

---
+++ while loop program/code05.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Print odd numbers between 1 and 9 using while loop */
+
+    int i = 1;
+
+    while (i <= 9) {
+        printf("%d\n", i);
+        i = i + 2;
+    }
+
+    return 0;
+}
+
