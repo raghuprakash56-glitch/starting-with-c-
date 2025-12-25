90cd1f7ad8499af8c2598af8f8c4b020408ec75e
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-25 23:03:43 +0530

    Add files via upload

---
+++ while loop program/code01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Print numbers from 5 to 1 using while loop */
+
+    int i = 5;
+
+    while (i >= 1) {
+        printf("%d\n", i);
+        i--;
+    }
+
+    return 0;
+}
