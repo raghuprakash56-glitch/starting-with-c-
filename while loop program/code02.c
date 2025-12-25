3387fb5496d5badf512334f91c01c22c798180a0
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-25 23:04:39 +0530

    Add files via upload

---
+++ while loop program/code02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Print numbers from 1 to 5 using while loop */
+
+    int i = 1;
+
+    while (i <= 5) {
+        printf("%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
