a43feb987c7be080efb5509e734656582ac7ff83
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-25 23:07:57 +0530

    Add files via upload

---
+++ while loop program/code06.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Print two-digit odd numbers below 20 using while loop */
+
+    int i = 11;
+
+    while (i < 20) {
+        printf("%d\n", i);
+        i = i + 2;
+    }
+
+    return 0;
+}
