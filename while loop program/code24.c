e7e7960f5f193f0c991acee844f94f865abf4c6d
Raghu Prakash.C <raghuprakash56@gmail.com>
2025-12-27 00:28:46 +0530

    Add files via upload

---
+++ while loop program/code24.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    Write a program to get two numbers from the user
+    and print the LCM of those numbers.
+    */
+
+    int a, b, max;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    /* Find maximum of a and b */
+    if (a > b)
+        max = a;
+    else
+        max = b;
+
+    /* Find LCM using while loop */
+    while (1)
+    {
+        if (max % a == 0 && max % b == 0)
+        {
+            printf("LCM = %d", max);
+            break;
+        }
+        max++;
+    }
+
+    return 0;
+}
