14db69b82140c0f003deab16413c0ec05618a171
Raghu Prakash.C <raghuprakash56@gmail.com>
2025-12-27 00:26:45 +0530

    Add files via upload

---
+++ while loop program/code18.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    Write a program to print the total number
+    of single digit prime numbers.
+    */
+
+    int num = 2;
+    int count = 0;
+    int i, isPrime;
+
+    while (num <= 9)
+    {
+        i = 2;
+        isPrime = 1;
+
+        while (i <= num / 2)
+        {
+            if (num % i == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+            i++;
+        }
+
+        if (isPrime == 1)
+        {
+            count++;
+        }
+
+        num++;
+    }
+
+    printf("Total number of single digit prime numbers = %d", count);
+
+    return 0;
+}
