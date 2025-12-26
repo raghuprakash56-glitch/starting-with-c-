17cbb29f6ecde6e41ccd194561a8f9eb59152f32
raghuprakash56-glitch <raghuprakash56@gmail.com>
2025-12-26 06:39:55 +0530

    Add files via upload

---
+++ while loop program/code07.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Print two-digit odd numbers whose sum of digits is 7 */
+
+    int num = 11;
+    int sum;
+
+    while (num < 100) {
+
+        if (num % 2 != 0) {
+            sum = (num / 10) + (num % 10);
+
+            if (sum == 7) {
+                printf("%d\n", num);
+            }
+        }
+
+        num++;
+    }
+
+    return 0;
+}
