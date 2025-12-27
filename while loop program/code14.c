99d4d9a77321e8860b6626fc8255caed62c58d2a
Raghu Prakash.C <raghuprakash56@gmail.com>
2025-12-27 10:38:26 +0530

    Update code14.c

--- while loop program/code14.c
+++ while loop program/code14.c
@@ -1,21 +1,17 @@
+ /* Check and print whether a number entered by the user is prime or not */
 #include <stdio.h>
-
 int main() {
-
-    /* Check and print whether a number entered by the user is prime or not */
-
     int num, i = 2, flag = 0;
-
     printf("Enter a number: ");
     scanf("%d", &num);
 
     if (num <= 1) {
-        flag = 1; // Numbers less than or equal to 1 are not prime
+        flag = 1;
     }
 
     while (i <= num / 2) {
         if (num % i == 0) {
-            flag = 1; // Found a divisor, not prime
+            flag = 1;
             break;
         }
         i++;
@@ -28,3 +24,4 @@ int main() {
 
     return 0;
 }
+
