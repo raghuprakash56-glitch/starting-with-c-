e46d3689b99355312f66b1af463812f273d2bda1
Raghu Prakash.C <raghuprakash56@gmail.com>
2025-12-27 10:36:57 +0530

    Update code13.c

--- while loop program/code13.c
+++ while loop program/code13.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
-
-int main() {
-
-    /* Get a number from user. 
+  /* Get a number from user. 
        If the last digit is even, print the number as it is.
        If the last digit is odd, subtract 1 from the last digit and print the number.
     */
 
+#include <stdio.h>
+int main() {
     int num, last_digit;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    last_digit = num % 10;  // Last digit (MSD)
-
-    if (last_digit % 2 != 0) { // If last digit is odd
-        num = num - 1;          // Subtract 1 from last digit
+    last_digit = num % 10; 
+    if (last_digit % 2 != 0) {
+        num = num - 1;          
     }
 
     printf("%d\n", num);
 
     return 0;
 }
+
