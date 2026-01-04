a8e3ade4de25e0fd8b386d6cdc8a4b30fd020644
Raghu Prakash.C <raghuprakash56@gmail.com>
2026-01-04 19:07:41 +0530

    Add files via upload

---
+++ basic of functions/ca23.c
@@ -0,0 +1,28 @@
+// Program to print total number of single-digit perfect square numbers using while loop and function
+#include <stdio.h>
+
+void disp_single_digit_square()
+{
+    int num, digit, count = 0;
+
+    scanf("%d", &num);
+
+    while (num != 0)
+    {
+
+        digit = num % 10;
+
+        if (digit == 0 || digit == 1 || digit == 4 || digit == 9)
+            count++;
+
+        num = num / 10;
+    }
+
+    printf("%d", count);
+}
+
+int main()
+{
+    disp_single_digit_square();
+    return 0;
+}
