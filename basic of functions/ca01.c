f25da0e6acc83370418c70cc18cc8a096094c3f9
Raghu Prakash.C <raghuprakash56@gmail.com>
2026-01-04 18:57:05 +0530

    Add files via upload

---
+++ basic of functions/ca01.c
@@ -0,0 +1,18 @@
+// Function to display numbers from 1 to 5 in ascending order using for loop
+#include <stdio.h>
+
+void disp_ascend();
+int main()
+{
+    disp_ascend();
+    return 0;
+}
+
+void disp_ascend()
+{
+    int i;
+    for (i = 1; i <= 5; i++)
+    {
+        printf("%d\n", i);
+    }
+}
