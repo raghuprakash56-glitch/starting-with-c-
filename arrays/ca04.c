aaa295d0cba5faf69263c30b70c67945212c2db0
Raghu Prakash.C <raghuprakash56@gmail.com>
2026-01-07 10:37:56 +0530

    Add files via upload

---
+++ arrays/ca04.c
@@ -0,0 +1,26 @@
+// Program to get 5 numbers from user and print the biggest number using array and for loop
+#include <stdio.h>
+
+int main()
+{
+    int a[5], i, max;
+
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    max = a[0];
+
+    for (i = 1; i < 5; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+    }
+
+    printf("%d", max);
+
+    return 0;
+}
