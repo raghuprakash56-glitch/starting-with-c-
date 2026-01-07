70db07509102355af4120ac7fdd462d8a5187f62
Raghu Prakash.C <raghuprakash56@gmail.com>
2026-01-07 10:34:38 +0530

    Add files via upload

---
+++ arrays/ca01.c
@@ -0,0 +1,21 @@
+// Program to get 5 numbers from user and print the sum of all numbers using array and for loop
+#include <stdio.h>
+
+int main()
+{
+    int a[5], i, sum = 0;
+
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
