1800fba499f5e368dffb15f25ee1105ad1b713d5
Raghu Prakash.C <raghuprakash56@gmail.com>
2026-01-07 10:43:30 +0530

    Add files via upload

---
+++ arrays/ca09.c
@@ -0,0 +1,31 @@
+// Program to get 5 numbers, remove even numbers, create a new array and print it
+#include <stdio.h>
+
+int main()
+{
+    int a[5], b[5], i, j = 0;
+
+    // Read 5 numbers from user
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    // Remove even numbers and store even numbers in new array
+    for (i = 0; i < 5; i++)
+    {
+        if (a[i] % 2 == 1)
+        {
+            b[j] = a[i];
+            j++;
+        }
+    }
+
+    // Print new array
+    for (i = 0; i < j; i++)
+    {
+        printf("%d ", b[i]);
+    }
+
+    return 0;
+}
