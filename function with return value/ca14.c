b550bc83bad54307759de67b5bffbc8baa61d7a8
Raghu Prakash.C <raghuprakash56@gmail.com>
2026-01-07 10:16:23 +0530

    Add files via upload

---
+++ function with return value/ca14.c
@@ -0,0 +1,35 @@
+// Program to print total number of two-digit perfect square numbers in a number using while loop
+#include <stdio.h>
+
+void disp_two_digit_square()
+{
+    int num, lastDigit, secondLastDigit, twoDigit, count = 0;
+
+    scanf("%d", &num);
+
+    lastDigit = num % 10;
+    num = num / 10;
+
+    while (num != 0)
+    {
+        secondLastDigit = num % 10;
+        twoDigit = secondLastDigit * 10 + lastDigit;
+
+        if (twoDigit == 16 || twoDigit == 25 || twoDigit == 36 ||
+            twoDigit == 49 || twoDigit == 64 || twoDigit == 81)
+        {
+            count++;
+        }
+
+        lastDigit = secondLastDigit;
+        num = num / 10;
+    }
+
+    printf("%d", count);
+}
+
+int main()
+{
+    disp_two_digit_square();
+    return 0;
+}
