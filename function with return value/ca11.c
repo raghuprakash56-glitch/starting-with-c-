e6032aa61249b1f0f3cebb10eb55381694489381
Raghu Prakash.C <raghuprakash56@gmail.com>
2026-01-07 10:12:10 +0530

    Add files via upload

---
+++ function with return value/ca11.c
@@ -0,0 +1,27 @@
+// Program to print total number of odd digits in a number using while loop
+#include <stdio.h>
+
+void disp_total_odd_digits()
+{
+    int num, digit, count = 0;
+
+    scanf("%d", &num);
+
+    while (num != 0)
+    {
+        digit = num % 10;
+
+        if (digit % 2 != 0)
+            count++;
+
+        num = num / 10;
+    }
+
+    printf("%d", count);
+}
+
+int main()
+{
+    disp_total_odd_digits();
+    return 0;
+}
